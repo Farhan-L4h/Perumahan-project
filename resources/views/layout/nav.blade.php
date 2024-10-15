0c05274ba5ea847bd1b5d17e642c5eda99d0f303
Farhan-L4h <muhanmedan06@gmail.com>
2024-10-15 09:08:03 +0700

    Add Template

---
+++ resources/views/layout/nav.blade.php
@@ -0,0 +1,33 @@
+<nav class="site-nav">
+      <div class="container">
+        <div class="menu-bg-wrap">
+          <div class="site-navigation">
+            <a href="/" class="logo m-0 float-start">Polinema Property</a>
+
+            <ul
+              class="js-clone-nav d-none d-lg-inline-block text-start site-menu float-end"
+            >
+              <li class="active"><a href="/">Home</a></li>
+              <li class="has-children">
+                <a href="/property">Properties</a>
+                <ul class="dropdown">
+                  <li><a href="/property-1">Buy Property</a></li>
+                  <li><a href="/property-1">Sell Property</a></li>
+                </ul>
+              </li>
+              <li><a href="/services">Services</a></li>
+              <li><a href="/about">About</a></li>
+              <li><a href="/contact">Contact Us</a></li>
+            </ul>
+
+            <a
+              href="#"
+              class="burger light me-auto float-end mt-1 site-menu-toggle js-menu-toggle d-inline-block d-lg-none"
+              data-toggle="collapse"
+              data-target="#main-navbar">
+              <span></span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
--- routes/web.php
+++ routes/web.php
@@ -2,40 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home', function () {
+    return view('user/index');
 });
 
 // Properties
 Route::get('/property', function () {
-    return view('properties');
+    return view('user/properties');
 });
 Route::get('/property-1', function () {
-    return view('property-single');
+    return view('user/property-single');
 });
 
 // service
 Route::get('/services', function () {
-    return view('services');
+    return view('user/services');
 });
 
 // about
 Route::get('/about', function () {
-    return view('about');
+    return view('user/about');
 });
 
 // about
 Route::get('/contact', function () {
-    return view('contact');
+    return view('user/contact');
 });
